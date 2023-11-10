1eb6d641f55352887d2d1d313a35884981ee0dd7
jaekkang <rkdwornjs123@gmail.com>
2023-11-10 18:46:20 +0900

    Refactor: Rename method

--- src/main/java/wanted/moneymwoni/app/category/service/CategoryService.java
+++ src/main/java/wanted/moneymwoni/app/category/service/CategoryService.java
@@ -1,11 +1,11 @@
 package wanted.moneymwoni.app.category.service;
 
+import wanted.moneymwoni.app.budget.domain.Budget;
 import wanted.moneymwoni.app.category.domain.Category;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> findAllCategory();
-
-
+    List<Category> findAll();
+    Long createCategory(Budget budget, String categoryName);
 }
