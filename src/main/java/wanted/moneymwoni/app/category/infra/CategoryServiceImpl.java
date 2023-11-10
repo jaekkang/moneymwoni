87f332664794c1cf00641d7633fa0c527c35a4a8
jaekkang <rkdwornjs123@gmail.com>
2023-11-10 18:46:05 +0900

    Refactor: Rename method & add create method

--- src/main/java/wanted/moneymwoni/app/category/infra/CategoryServiceImpl.java
+++ src/main/java/wanted/moneymwoni/app/category/infra/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package wanted.moneymwoni.app.category.infra;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import wanted.moneymwoni.app.budget.domain.Budget;
 import wanted.moneymwoni.app.category.domain.Category;
 import wanted.moneymwoni.app.category.domain.CategoryRepository;
 import wanted.moneymwoni.app.category.service.CategoryService;
@@ -15,7 +16,18 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Override
-    public List<Category> findAllCategory() {
+    public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    @Override
+    public Long createCategory(Budget budget, String categoryName) {
+        Category newCategory = Category.builder()
+                                   .name(categoryName)
+                                   .budget(budget)
+                                   .build();
+        categoryRepository.save(newCategory);
+
+        return newCategory.getId();
     }
 }
