409308975d5cb03e260789ac3685c5477b7e8ae5
jaekkang <rkdwornjs123@gmail.com>
2023-11-12 14:27:27 +0900

    Feat: add search method ( by month) in budget repository

--- src/main/java/wanted/moneymwoni/app/budget/domain/BudgetRepository.java
+++ src/main/java/wanted/moneymwoni/app/budget/domain/BudgetRepository.java
@@ -1,6 +1,13 @@
 package wanted.moneymwoni.app.budget.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import wanted.moneymwoni.app.member.domain.Member;
+
+import java.util.Optional;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    @Query("SELECT b FROM Budget b WHERE EXTRACT(YEAR FROM b.year) = :year AND EXTRACT(MONTH FROM b.year) = :month AND b.member = :member")
+    Optional<Budget> findByYearAndMonthAndMember(@Param("year") int year, @Param("month") int month, @Param("member") Member member);
 }
